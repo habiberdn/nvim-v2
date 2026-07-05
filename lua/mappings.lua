723429d171677e5a46d11c75b873e4c01a93336c
habiberdn <habib.erdian@gmail.com>
2026-07-05 17:09:41 +0700

    new config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -23,3 +23,14 @@ map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Window/Pane Left" })
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Window/Pane Down" })
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Window/Pane Up" })
 map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Window/Pane Right" })
+
+map("n", "<C-Up>", "v<Up>", { desc = "Block Up" })
+map("n", "<C-Down>", "v<Down>", { desc = "Block Down" })
+map("n", "<C-Left>", "v<Left>", { desc = "Block Left" })
+map("n", "<C-Right>", "v<Right>", { desc = "Block Right" })
+
+-- Meneruskan seleksi saat sudah di dalam Visual Mode
+map("v", "<C-Up>", "<Up>", { desc = "Extend Block Up" })
+map("v", "<C-Down>", "<Down>", { desc = "Extend Block Down" })
+map("v", "<C-Left>", "<Left>", { desc = "Extend Block Left" })
+map("v", "<C-Right>", "<Right>", { desc = "Extend Block Right" })
